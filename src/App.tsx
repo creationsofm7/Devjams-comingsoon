ffbdb98681b11200aed8b88c448a5feb6851d36e
GHOST . <muditpanday17@gmail.com>
2024-07-08 23:53:19 +0530

    navbar without assets added

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/navbar/navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='flex flex-1 place-content-center h-screen'>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <Navbar />
+    <div className='h-screen '>
+      <h1>hello
+      </h1>
+    </div>
+      
+      
     </>
   )
 }
